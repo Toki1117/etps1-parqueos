727e438e0c152b969018e6ae0c9e2fd96383e1c1
Richard <ricardochicas95@gmail.com>
2018-12-13 02:01:05 -0600

    listar_Buscar_parqueos

--- listar_parqueo.php
+++ listar_parqueo.php
@@ -9,26 +9,27 @@
 </head>
 <body>
 	<form action="" method="POST">
-	<input type="text" name="nombreEdificio">
+	<input type="text" name="nombreParqueo">
 	<input type="submit" value="Buscar" >
 	</form>
 
 <table   id="table" class="table">
 		<tr  class ="" id="titulo">
-			<td>Nombre de edificio</td>
-			<td>Nombre corto </td>
+			<td>Nombre parqueo</td>
+			<td>Cantidad</td>
+            <td>Reservados</td>
 		</tr>
 
 
     <?php
-		if(isset($_POST['nombreEdificio']))
+		if(isset($_POST['nombreParqueo']))
 		{
-		$buscar = $_POST['nombreEdificio'];
-		$url = "http://localhost:8080/etps1/ProyectoParqueo/welcome/get_edificio_por_nombre";
+		$buscar = $_POST['nombreParqueo'];
+		$url = "http://localhost:8080/etps1/ProyectoParqueo/welcome/get_parqueo_por_nombre";
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "nombreEdificio=$buscar");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "nombreParqueo=$buscar");
 		$response = curl_exec($ch);
 		$array = json_decode($response, true);
 		$cont = count ($array);
@@ -37,12 +38,13 @@
 		for ($i = 0; $i< $cont; $i++)
 		{
 			echo"<tr class =''>";
-				echo"<td>",$array[$i]['nombre_edificio'],"</td>";
-				echo"<td>",$array[$i]['nombre_corto_edificio'],"</td>";
+				echo"<td>",$array[$i]['nombre_parqueo'],"</td>";
+                echo"<td>",$array[$i]['cantidad_parqueo'],"</td>";
+                echo"<td>",$array[$i]['reservados_parqueo'],"</td>";
 			echo"</tr>";
 		}
 	}else{
-		$url = "http://localhost:8080/etps1/ProyectoParqueo/welcome/crud_listar_edificio";
+		$url = "http://localhost:8080/etps1/ProyectoParqueo/welcome/crud_listar_parqueo";
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$response = curl_exec($ch);
@@ -51,8 +53,9 @@
 		for ($i = 0; $i< $cont; $i++)
 		{
 			echo"<tr class =''>";
-				echo"<td>",$array[$i]['nombre_edificio'],"</td>";
-				echo"<td>",$array[$i]['nombre_corto_edificio'],"</td>";
+            echo"<td>",$array[$i]['nombre_parqueo'],"</td>";
+            echo"<td>",$array[$i]['cantidad_parqueo'],"</td>";
+            echo"<td>",$array[$i]['reservados_parqueo'],"</td>";
 			echo"</tr>";
 		}
 	}
